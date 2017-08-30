c603e3a0abf86cb8ff67f6402057143c0ee1559e
soong@company@shi6w1120 <songxinkun2003@sina.com>
2017-08-30 12:39:31 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 // @flow
+
+// https://flow.org/en/docs/usage/
+// https://flow.org/en/docs/lang/
+// https://flow.org/en/docs/lang/types-and-expressions/
 function square(n: number): number {
   return n * n;
+}
+
+function foo(x: ?number): string {
+  if (x) {
+    return x;
+  }
+  return "default string";
 }
 
 //square("2"); // Error!
